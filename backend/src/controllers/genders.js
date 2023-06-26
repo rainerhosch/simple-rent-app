c2047ec5066c11103337e5bbd3a580d350590e2d
rzoktan <rz.oktan@gmail.com>
2023-06-26 18:06:57 +0700

    update

--- backend/src/controllers/genders.js
+++ backend/src/controllers/genders.js
@@ -1,38 +1,6 @@
 const genderModel = require("../schemas/genders");
 const responseHelper = require("../helpers/sendResponse");
 
-// // Method for create gender datas
-// const genderCreate = (req, res) => {
-//   try {
-//     const dataToSave = genderModel.insertMany(req.body);
-//     res.status(200).json({
-//       message: "success create data",
-//       data: dataToSave,
-//     });
-//   } catch (error) {
-//     res.status(400).json({
-//       message: error.message,
-//       data: null,
-//     });
-//   }
-// };
-
-// // Method for find gender datas
-// const genderData = (req, res) => {};
-
-// // Method for edit gender datas
-// const genderEdit = (req, res) => {};
-
-// // Method for delete gender datas
-// const genderDelete = (req, res) => {};
-
-// module.exports = {
-//   genderCreate,
-//   genderData,
-//   genderEdit,
-//   genderDelete,
-// };
-// Method for create gender datas
 const createGenders = async (req, res) => {
   try {
     const fieldFilter = req.body.email;
